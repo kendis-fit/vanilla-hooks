ea72979f6d39e5f3ac9659e509f89af152b0d339
Kurt Endless <kendis_it@hotmail.com>
2020-06-30 12:01:32 +0300

    add: new parameters in useVanillaForm

--- index.d.ts
+++ index.d.ts
@@ -17,6 +17,7 @@ declare module "vanilla-hooks" {
     interface IForm<T>{
         schema: ObjectSchema<Shape<any, any>>;
         initialValues?: T;
+        validateChange?: boolean;
         valuesAnyway?: boolean;
         allFieldsExisted?: boolean;
         onSubmit: (isValid: boolean, values: T, errors?: any) => void;
@@ -25,7 +26,8 @@ declare module "vanilla-hooks" {
     interface IFormResult<T> {
         errors: DynamicObjectErrors;
         handleSubmit: () => void;
-        getValuest(): T;
+        setValues: () => void;
+        getValues: () => T;
     }
 
     function useVanillaForm<T>(form: IForm<T>): IFormResult<T>;

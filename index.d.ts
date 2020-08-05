b2c8e5405bff5389709e9992c7851cb958725ee0
Kurt Endless <kendis_it@hotmail.com>
2020-08-05 17:02:16 +0300

    update: fixed bug with babel

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+declare module "vanilla-hooks" {
+
+    import { ObjectSchema, Shape } from "yup";
+    import { Dispatch, SetStateAction } from "react";
+
+    type DynamicObjectErrors = { [key: string]: string };
+
+    interface IData<T> {
+        data: T;
+        setData: Dispatch<SetStateAction<T | undefined>>;
+        loading: boolean;
+        error: Error;
+    }
+
+    function useVanillaFetch<T>(fetchMethod: () => Promise<T>): IData<T>;
+
+    interface IForm<T>{
+        schema: ObjectSchema<Shape<any, any>>;
+        initialValues?: T;
+        valuesAnyway?: boolean;
+        allFieldsExisted?: boolean;
+        onSubmit: (isValid: boolean, values: T, errors?: any) => void;
+    }
+
+    interface IFormResult<T> {
+        errors: DynamicObjectErrors;
+        handleSubmit: () => void;
+        setValues: () => void;
+        getValues: () => T;
+    }
+
+    function useVanillaForm<T>(form: IForm<T>): IFormResult<T>;
+}
